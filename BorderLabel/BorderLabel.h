2b8fd4e0c1aa479af63012af5574c5cc50d90caa
pengpengduduguagua <2979468570@qq.com>
2019-03-05 16:28:53 +0800

    first commit

---
+++ BorderLabel/BorderLabel.h
@@ -0,0 +1,13 @@
+//
+//  BorderLabel.h
+//  BorderLabel
+//
+//  Created by admin on 2019/3/5.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BorderLabel : UILabel
+
+@end
---
+++ BorderLabel/BorderLabel.m
@@ -0,0 +1,21 @@
+//
+//  BorderLabel.m
+//  BorderLabel
+//
+//  Created by admin on 2019/3/5.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import "BorderLabel.h"
+
+@implementation BorderLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
